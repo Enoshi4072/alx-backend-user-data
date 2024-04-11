cd45b7504a4c411d719406347ed476fb0d763a1e
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-11 09:59:56 +0000

    User_pwd

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,10 +7,10 @@ import typing
 def hash_password(password: str) -> bytes:
     """ Hashes a password using bcrypt """
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
 
 def is_valid(hashed_password: bytes, password: str):
     """ Check is plaintext password matches a hashed password """
-    return bcrypt.checkpw(password.encode(), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
