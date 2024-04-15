9cd862ba38ad5c0892776f70a269f1cf014442ff
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-15 15:06:34 +0000

    6. Basic auth

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """ Aborting the process """
+    abort(401)
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """ Returns the forbidden error """
+    abort(403)
