faa6c4198bae2aa5720271232155c359ebac6894
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-16 08:29:45 +0000

    authicate

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,9 +25,12 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
-    """ Aborting the process """
+@app_views.route('/unauthorized/', methods=["GET"], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return
+    - Aborting the process """
     abort(401)
 
 @app_views.route('/forbidden/', strict_slashes=False)
