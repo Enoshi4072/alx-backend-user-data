7181a34659fe94254955db2db13bb9e06e8eb4d2
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-19 13:01:36 +0000

    exp

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,6 +23,8 @@ if auth_type in ("some_auth", "basic_auth"):
     auth = Auth() if auth_type == "some_auth" else BasicAuth()
 elif auth_type == "session_auth":
     auth =  SessionAuth()
+if auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
 
 
 @app.before_request
