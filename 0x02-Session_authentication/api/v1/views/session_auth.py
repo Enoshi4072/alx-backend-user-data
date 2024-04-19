015aec2ea8e62c4491769bf40d9316d480d23a83
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-19 07:15:56 +0000

    session auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,46 +1,29 @@
-from flask import jsonify, request
+#!/usr/bin/env python3
+""" Handling all routes for session auth """
 from models.user import User
 from api.v1.views import app_views
-from api.v1.app import auth
+
+import os
 from typing import Tuple
-@app_views.route('/auth_session/login', methods=['POST', 'GET'], strict_slashes=False)
+from flask import abort, jsonify, request
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> Tuple[str, int]:
-    """Handles user login using Session authentication."""
-    if request.method == 'POST':
-        # Retrieve email and password parameters from form data
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        # Check if email is missing or empty
-        if not email:
-            return jsonify({'error': 'email missing'}), 400
-
-        # Check if password is missing or empty
-        if not password:
-            return jsonify({'error': 'password missing'}), 400
-
-        # Retrieve User instance based on the email
-        user = User.search({'email': email})
-
-        # If no User found, return error
-        if not user:
-            return jsonify({'error': 'no user found for this email'}), 404
-
-        # Check if the provided password is correct
-        if not user.is_valid_password(password):
-            return jsonify({'error': 'wrong password'}), 401
-
-        # Create a Session ID for the User ID
-        session_id = auth.create_session(user.id)
-
-        # Return the dictionary representation of the User
-        user_json = user.to_json()
-
-        # Set the cookie to the response
-        response = jsonify(user_json)
-        response.set_cookie(auth.session_cookie_name, session_id)
-
-        return response, 200
-    else:
-        # If method is not allowed, return error
-        return jsonify({'error': 'Method Not Allowed'}), 40i5
+    """ Handling login requests """
+    er_msg = {'error': 'no user found for this email'}
+    email = request.form.get('email')
+    if email is None or len(email.strip()) == 0:
+        return jsonify({'error': 'email missing'}), 400
+    password = request.form.get('password')
+    if password is None or len(password.strip()) == 0:
+        return jsonify({'error': 'password missing'}), 400
+    users = User.search({'email': email})
+    if len(users) <= 0:
+        return jsonify(er_msg), 404
+    if users[0].is_valid_password(password):
+        from api.v1.app import auth
+        session_id = auth.create_session(getattr(users[0], 'id'))
+        response = jsonify(users[0].to_json())
+        response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+        return response
+    return jsonify({'error': 'wrong password'}), 401
