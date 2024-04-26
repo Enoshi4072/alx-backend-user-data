a8a5e4dfde483146ebe0b0312b5fcd87d3132293
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-26 04:10:07 +0000

    add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -11,7 +11,6 @@ from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
-
 class DB:
     """ DB class """
     def __init__(self) -> None:
@@ -31,40 +30,37 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds a user to the table """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
         return user
-    
+
     def find_user_by(self, **kwargs) -> User:
         """ Finds a user in the table """
-        #create a query to find users based on the provided filters
         query = self._session.query(User)
         for key, value in kwargs.items():
-            #check if the key exists as an attr in the user class
             if key in User.__dict__:
                 query = query.filter_by(**{key: value})
             else:
                 raise InvalidRequestError
-        #Execute the query and retrieve the first matching user
         result = query.first()
         if result is None:
             raise NoResultFound
         return result
-   
+
     def update_user(self, user_id: int, **kwargs) -> None:
         """ Updates the user """
         try:
-            #find the user by user_id
             user = self.find_user_by(id=user_id)
-            #Update the user's attrs
             for key, value in kwargs.items():
                 if hasattr(user, key):
                     setattr(user, key, value)
                 else:
                     raise ValueError
-            #Commit changes to the db
             self._session.commit()
         except NoResultFound:
             raise ValueError
-
