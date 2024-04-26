de9850a70f54aeba39bccba39a1344e017448dfa
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-26 03:58:23 +0000

    User

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
+
 class User(Base):
+    """ Creating the base class """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
