092cc31a8908aaa57e52ee6b94c9ef5784e9f351
Enoshi4072 <enockkibet6642@gmail.com>
2024-04-26 05:12:33 +0000

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,13 +50,11 @@ def logout() -> str:
     """ Logs out a user from the session """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user is not None:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
+    if user is None:
         abort(403)
-
-
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+    
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
     """ Handling a user profile """
